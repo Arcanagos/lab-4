4732705ef244fe62cd9911327a91fe3ef8e44349
Rafał Kwiecień <53783787+Arcanagos@users.noreply.github.com>
2022-01-16 19:14:57 +0100

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,85 @@
+﻿using lab4.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab4.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly DatabaseContext _context;
+
+        public HomeController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var users = from m in _context.Users
+                         select m;
+
+            return View(users.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("ID,FirstName,LastName,Age,City")] DatabaseClass users)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(users);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        public async Task<IActionResult> Edit(int id)
+        {
+            var users = await _context.Users.FindAsync(id);
+            if (users == null)
+            {
+                return NotFound();
+            }
+            return View(users);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,FirstName,LastName,Age,City")] DatabaseClass users)
+        {
+            if (id != users.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Update(users);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        [ActionName("Delete")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var users = await _context.Users.FindAsync(id);
+            _context.Users.Remove(users);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/DBSeed.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab4.Models
+{
+    public class DBSeed
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new DatabaseContext(serviceProvider.GetRequiredService<DbContextOptions<DatabaseContext>>()))
+            {
+                if (context.Users.Any())
+                {
+                    return;
+                }
+
+                context.Users.Add(new DatabaseClass
+                {
+                    FirstName = "Jan",
+                    LastName = "Rodzyń",
+                    Age = 62,
+                    City = "Warsaw"
+                });
+                context.Users.Add(new DatabaseClass
+                {
+                    FirstName = "Adam",
+                    LastName = "Grudziński",
+                    Age = 29,
+                    City = "Cracov"
+                });
+                context.Users.Add(new DatabaseClass
+                {
+                    FirstName = "Rafał",
+                    LastName = "Kalasza",
+                    Age = 22,
+                    City = "Lipinki Łużyckie"
+                });
+                context.Users.Add(new DatabaseClass
+                {
+                    FirstName = "Barbara",
+                    LastName = "Kaszub",
+                    Age = 25,
+                    City = "Legnica"
+                });
+                context.Users.Add(new DatabaseClass
+                {
+                    FirstName = "Anna",
+                    LastName = "Krokiet",
+                    Age = 36,
+                    City = "Leipzig"
+                });
+                context.SaveChanges();
+            }
+        }
+    }
+}
